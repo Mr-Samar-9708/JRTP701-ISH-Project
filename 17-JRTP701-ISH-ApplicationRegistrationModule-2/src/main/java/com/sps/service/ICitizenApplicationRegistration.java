f9ebc09adb14744c05b828ecdc6a717753a38f2d
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2024-10-29 12:18:36 +0530

    Delete 17-JRTP701-ISH-ApplicationRegistrationModule-2 directory

--- 17-JRTP701-ISH-ApplicationRegistrationModule-2/src/main/java/com/sps/service/ICitizenApplicationRegistration.java
+++
@@ -1,8 +0,0 @@
-package com.sps.service;
-
-import com.sps.bindigData.AppRegistrationInput;
-
-public interface ICitizenApplicationRegistration {
-
-	public Integer citizenRegistration(AppRegistrationInput ssn);
-}
