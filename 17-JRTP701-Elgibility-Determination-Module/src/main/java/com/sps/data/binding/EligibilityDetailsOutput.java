c033469d12c1bbb688a944f0519d7394acb9f4c0
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2024-10-29 12:19:20 +0530

    Delete 17-JRTP701-Elgibility-Determination-Module directory

--- 17-JRTP701-Elgibility-Determination-Module/src/main/java/com/sps/data/binding/EligibilityDetailsOutput.java
+++
@@ -1,9 +0,0 @@
-package com.sps.data.binding;
-
-import lombok.Data;
-
-@Data
-public class EligibilityDetailsOutput {
-	
-	
-}
--- 17-JRTP701-Elgibility-Determination-Module/src/main/java/com/sps/repository/IDcCaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sps.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sps.entity.DcCaseEntity;
-
-public interface IDcCaseRepository extends JpaRepository<DcCaseEntity, Integer> {
-
-}
